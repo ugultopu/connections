c2fcb62a614f5f44822b06b6124d771c0cf1dc4e
Utku <ugultopu@gmail.com>
2020-04-11 17:15:47 -0300

    Get 'test_mutual_friends' working
    Added a member function named 'mutual_friends' in the Person model.

--- connections/models/person.py
+++ connections/models/person.py
@@ -10,7 +10,8 @@ class Person(Model, CRUDMixin, CreatedUpdatedMixin):
 
     connections = db.relationship('Connection', foreign_keys='Connection.from_person_id')
 
-    def mutual_friends(self, other_person):
-        other_person_friends = [c.to_person for c in other_person.connections if c.connection_type is ConnectionType.friend]
-        my_friends = [c.to_person for c in self.connections if c.connection_type is ConnectionType.friend]
-        return [f for f in my_friends if f in other_person_friends]
+    def friends(self):
+        return {c.to_person for c in self.connections if c.connection_type is ConnectionType.friend}
+
+    def mutual_friends(self, other):
+        return self.friends() & other.friends()
